064b4341e9c8d480cbe9589ebe5e57f21b8b42d6
caro8847 <carolinestiepernielsen@gmail.com>
2025-10-15 12:22:05 +0200

    footer 12.22

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,22 @@
-document.getElementById(footer-right)
+console.log('it works!');
+
+document.getElementById('footer-right');
+
+const socials = [
+    {
+        href:   "https://www.instagram.com/ventilendanmark/",
+        icon:   "fa-brands fa-instagram",
+        alt:    "Instagram icon"
+    },
+    {
+        href:   "https://www.facebook.com/ventilendanmark/",
+        icon:   "fa-brands fa-facebook-f",
+        alt:    "FaceBook icon"
+    }
+]
+
+for (let i = 0; socials.length; i++) {
+    
+}
+
+
